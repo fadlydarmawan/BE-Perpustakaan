d6f3f0d7ef9dcbb3f29224bbab99f5234de6126b
Fadlydarmawan <fadlydarmawan0@gmail.com>
2024-10-16 19:35:40 +0700

    updated

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,15 @@
+import {Request, response, Response} from "express";
+import dotenv from "dotenv";
+import { request } from "http";
+
+
+dotenv.config();
+export class middleware {
+@param req
+@param res
+}
+
+public async authenticatedToken(
+    req: Request,
+    res: Response
+)
